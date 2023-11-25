e463fc74d55a951b5a6ba53aff1b33ceb866a5db
psilvac <126999557+psilvac@users.noreply.github.com>
2023-11-25 11:06:55 -0300

    Update numeros.py

--- numeros.py
+++ numeros.py
@@ -6,4 +6,4 @@ with open(file_path, 'w') as file:
     for i in range(1, 100001):
         file.write(str(i) + '\n')
 
-print(f'Se ha creado el archivo {file_path} con numeros del 1 al 100,000.')
+print('Se ha creado el archivo con numeros del 1 al 100,000')
