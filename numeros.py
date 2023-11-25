2c08dbec9acfbd75a7c53bb0ac952e5f45921d48
psilvac <126999557+psilvac@users.noreply.github.com>
2023-11-25 11:19:56 -0300

    Update acentos numeros.py

--- numeros.py
+++ numeros.py
@@ -18,6 +18,6 @@ print('Se ha creado el archivo con numeros del 1 al 100,000')
 # Detiene el cronometro
 end_time = time.time()
 
-# Calcula y muestra el tiempo de ejecución
+# Calcula y muestra el tiempo de ejecucion
 execution_time = end_time - start_time
 print(f'Tiempo de ejecucion: {execution_time} segundos')
