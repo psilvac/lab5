516732f70f228cd3ca6666e1b3a9ebbdc7ac97a1
psilvac <126999557+psilvac@users.noreply.github.com>
2023-11-25 20:58:26 -0300

    Add files via upload

---
+++ numeros.py
@@ -0,0 +1,16 @@
+import time
+
+# Obten el tiempo de inicio
+inicio_tiempo = time.time()
+
+with open('numeros.txt', 'w') as archivo:
+    for numero in range(1, 100001):
+        archivo.write(str(numero) + '\n')
+
+# Obten el tiempo de finalizacion
+fin_tiempo = time.time()
+
+# Calcula el tiempo total de ejecucion
+tiempo_total = fin_tiempo - inicio_tiempo
+
+print(f"Archivo creado exitosamente en {tiempo_total} segundos.")
