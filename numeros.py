d8ac8f01d56941bb76ae35354a47bc4ffeb0cd67
psilvac <126999557+psilvac@users.noreply.github.com>
2023-11-25 11:05:10 -0300

    Update numeros.py

--- numeros.py
+++ numeros.py
@@ -1,9 +1,9 @@
-# Código para generar un archivo con números del 1 al 100,000
+# Codigo para generar un archivo con numeros del 1 al 100,000
 
-file_path = '/home/ec2-user/numeros.txt'  # Cambia esto según la estructura de tu directorio
+file_path = '/home/ec2-user/numeros.txt'  # Cambia esto segun la estructura de tu directorio
 
 with open(file_path, 'w') as file:
     for i in range(1, 100001):
         file.write(str(i) + '\n')
 
-print(f'Se ha creado el archivo {file_path} con números del 1 al 100,000.')
+print(f'Se ha creado el archivo {file_path} con numeros del 1 al 100,000.')
